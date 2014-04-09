5b947974f8d4cc6cb7d4803af4adbd2618fa4602
David Turbay <davidturbay2@gmail.com>
2014-04-09 01:30:51 -0400

    additions

---
+++ nytime_data/lab01.py
@@ -0,0 +1,12 @@
+import json
+
+nytimes = json.load(open('malaysia_pretty_printed.json', 'r'))
+print ','.join(['contributor','pub date','section','subsection','wordcount','url'])
+
+for article in nytimes['response']['docs']:
+	print '"' + '","'.join([article['byline']['contributor'],
+		article['pub_date'],
+        article['section_name'],
+        article['subsection_name'],
+        article['word_count'],
+        article['web_url']]) + '"'
