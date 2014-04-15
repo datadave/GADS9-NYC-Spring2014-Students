6aa0daef256402a517418713fcdac460031fe591
JustinGA <justinkemmerer@gmail.com>
2014-04-15 18:55:43 -0400

    adjusted lab02

--- lab_submissions/lab02/justin/untitled.py
+++ lab_submissions/lab02/justin/untitled.py
@@ -2,7 +2,10 @@ class Car():
 	def __init__(self, model='Ford', wheels=4):
 		self.model = model
 		self.running = False
-		self.wheels = wheels
+		if wheels < 4:
+			raise Exception("Your car must have at least 4 wheels")
+		else:
+			self.wheels = wheels
 	def start(self):
 		if self.running != True:
 			print 'The car started!'
